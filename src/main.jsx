7e4b7014903f119e8e14efd58e741a16de975105
ishaan1011 <110586493+ishaan1011@users.noreply.github.com>
2025-09-06 02:53:09 -0700

    Adding site analytics using Vercel

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./assets/index.css";
 import { GlobalStyles } from "./utils";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
     <GlobalStyles />
+    <Analytics />
   </React.StrictMode>
 );
